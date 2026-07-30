66dc0287e43b154f1e6f20c4efde4b7208b48df8
Sometireguy <charleswrhyne@gmail.com>
2026-07-30 12:35:02 -0500

    Delete index.jsx

--- index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import WMSBinTracker from './RackTrack_1';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WMSBinTracker />
-  </React.StrictMode>,
-);
