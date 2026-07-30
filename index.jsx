feb48e59dccbe50e68cb5d15d1bbbfbbdc0d3adc
Sometireguy <charleswrhyne@gmail.com>
2026-07-30 11:07:33 -0500

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WMSBinTracker from './RackTrack_1';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WMSBinTracker />
+  </React.StrictMode>,
+);
